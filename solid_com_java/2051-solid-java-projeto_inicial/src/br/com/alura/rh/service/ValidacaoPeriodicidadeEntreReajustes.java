c783cc5f18cbecb47ab80e898c1989a805fd1245
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2022-02-06 15:59:13 -0300

    Extraindo validações.

---
+++ solid_com_java/2051-solid-java-projeto_inicial/src/br/com/alura/rh/service/ValidacaoPeriodicidadeEntreReajustes.java
@@ -0,0 +1,22 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ValidacaoPeriodicidadeEntreReajustes implements ValidacaoReajuste{
+
+    @Override
+    public void validar(Funcionario funcionario, BigDecimal aumento) {
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+        long mesesDeseUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+        if (mesesDeseUltimoReajuste < 6) {
+            throw new ValidacaoException("Intervalo entre reajustes deve ser no minimo de 6 meses");
+        }
+    }
+}
