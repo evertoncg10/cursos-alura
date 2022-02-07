a777d7f88d9b8f9b6b4a7f340b371d37e0e9f747
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2022-02-07 17:39:53 -0300

    Refatoração de melhorias

--- solid_com_java/2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/Funcionario.java
+++ solid_com_java/2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/Funcionario.java
@@ -1,24 +1,16 @@
 package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
-
-import br.com.alura.rh.ValidacaoException;
 
 public class Funcionario {
 
-	private String nome;
-	private String cpf;
-	private Cargo cargo;
+	private DadosPessoais dadosPessoais;
 	private BigDecimal salario;
 	private LocalDate dataUltimoReajuste;
 
-	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.cargo = cargo;
-		this.salario = salario;
+	public Funcionario(DadosPessoais dadosPessoais) {
+		this.dadosPessoais = dadosPessoais;
 	}
 
 	public void atualizarSalario(BigDecimal salarioReajustado) {
@@ -26,28 +18,12 @@ public class Funcionario {
 		this.dataUltimoReajuste = LocalDate.now();
 	}
 
-	public String getNome() {
-		return nome;
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public Cargo getCargo() {
-		return cargo;
-	}
-
-	public void setCargo(Cargo cargo) {
-		this.cargo = cargo;
+	public void setDadosPessoais(DadosPessoais dadosPessoais) {
+		this.dadosPessoais = dadosPessoais;
 	}
 
 	public BigDecimal getSalario() {
@@ -67,6 +43,6 @@ public class Funcionario {
 	}
 
 	public void promover(Cargo novoCargo) {
-		this.cargo = novoCargo;
+		dadosPessoais.setCargo(novoCargo);
 	}
 }
